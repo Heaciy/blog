07745e3ec4d2bb89724cdef2dcc6ce84e60fab9b
Heaciy <duke3247561384@icloud.com>
2020-09-12 20:36:25 +0800

    重写get_queryset方法使非管理员用户只能看见自己的文章

--- myblog/admin.py
+++ myblog/admin.py
@@ -9,19 +9,28 @@ class CategoryAdmin(admin.ModelAdmin):
 
 
 @admin.register(Tag)
-class CategoryAdmin(admin.ModelAdmin):
+class TagAdmin(admin.ModelAdmin):
     list_display = ('name', 'id')
 
 
 @admin.register(Blog)
 class BlogAdmin(admin.ModelAdmin):
     list_display = ['title', 'id', 'author', 'get_click_nums', 'category', 'create_time', 'modify_time']
+    search_fields = ['title', 'content']
 
     def formfield_for_foreignkey(self, db_field, request=None, **kwargs):
         """对外键进行设置"""
-        if db_field.name == 'author':
-            kwargs['initial'] = request.user.id
-            kwargs['queryset'] = User.objects.filter(username=request.user.username)
+        if not request.user.is_superuser:
+            if db_field.name == 'author':
+                kwargs['initial'] = request.user.id  # 默认为当前用户
+                kwargs['queryset'] = User.objects.filter(username=request.user.username)
         return super(BlogAdmin, self).formfield_for_foreignkey(
             db_field, request, **kwargs
         )
+
+    def get_queryset(self, request):
+        """如果不是超级管理员则只能看见自己的文章"""
+        qs = super(BlogAdmin, self).get_queryset(request)
+        if not request.user.is_superuser:
+            return qs.filter(author=request.user)
+        return qs
